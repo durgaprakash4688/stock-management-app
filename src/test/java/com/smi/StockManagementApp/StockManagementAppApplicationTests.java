aceebd15902d419568d2c97f4b7d62e078955dc1
Durga Prakash <c.durgaprakash@yahoo.com>
2022-01-24 23:04:02 -0500

    Initial Commit

---
+++ src/test/java/com/smi/StockManagementApp/StockManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smi.StockManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
